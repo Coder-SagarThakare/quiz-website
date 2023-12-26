3eed1ee2c45f1af94990ca2b922d9b4dfad2ae64
Coder-SagarThakare <saggythakare01@gmail.com>
2023-12-27 01:07:41 +0530

    index.js updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as SearchBar } from "./searchbar/SearchBar";
 export { default as Sidebar } from "./sidebar/Sidebar";
-export {default as Carousel} from './carousel/Carousel'
+export { default as Carousel } from './carousel/Carousel'
+export { default as Footer } from './footer/Footer'
