c5c8b4bfbb9fd2b869840d853f65931604bb2e4e
keshav halade <keshavhalade2019@gmail.com>
2024-01-05 12:40:12 +0530

    edited  signupform components

--- src/auth/forms/SignUpForm.jsx
+++ src/auth/forms/SignUpForm.jsx
@@ -1,33 +1,117 @@
-import React from 'react'
+// import React from "react";
+
+// function SignUpForm() {
+//   return (
+//     <div>
+//       <h2>Sign Up</h2>
+//       <form>
+//         <div>
+//           <label htmlFor="fullname">Full Name </label>
+//           <input type="text" name="fullname" />
+//         </div>
+
+//         <div>
+//           <label htmlFor="">Email address </label>
+//           <input type="email" name="email" id="email" />
+//         </div>
+
+//         <div>
+//           <label htmlFor="password">Password</label>
+//           <input type="password" name="password" id="" />
+//         </div>
+
+//         <div>
+//           <label htmlFor="confirm-password">Confirm Password</label>
+//           <input type="password" name="confirm-password" id="" />
+//         </div>
+
+//         <div>
+//           <input type="radio" name="gender-male" id="gender-male" />{" "}
+//           <label htmlFor="gender-male">Male</label>
+//           <input type="radio" name="gender-female" id="gender-female" />{" "}
+//           <label htmlFor="gender-female">Female</label>
+//         </div>
+
+//         <div>
+//           <button type="reset">Reset</button>
+//           <button>Sign Up</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default SignUpForm;
+
+import React from "react";
 
 function SignUpForm() {
   return (
-    <div>
-        <h2>Sign Up</h2>
-        <form>
-            <div>
-                <label htmlFor="">Email address </label>
-                <input type="email" name="email" id="email" />
-            </div>
-            
-            <div>
-                <label htmlFor="username">User name </label>
-                <input type="text" name='username'/>
-            </div>
+    <div className="container mt-5">
+      <h2>Sign Up</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="fullname">Full Name </label>
+          <input type="text" className="form-control" name="fullname" />
+        </div>
 
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="" />
-            </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address </label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            id="email"
+          />
+        </div>
 
-            <div>
-                <button>Sign Up</button>
-            </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" name="password" />
+        </div>
 
-             
-        </form>
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="confirm-password"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="radio"
+            className="form-check-input"
+            name="gender"
+            id="gender-male"
+          />
+          <label className="form-check-label" htmlFor="gender-male">
+            Male
+          </label>
+
+          <input
+            type="radio"
+            className="form-check-input"
+            name="gender"
+            id="gender-female"
+          />
+          <label className="form-check-label" htmlFor="gender-female">
+            Female
+          </label>
+        </div>
+
+        <div className="form-group">
+          <button type="reset" className="btn btn-secondary">
+            Reset
+          </button>
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUpForm
+export default SignUpForm;
