6457fd89d8c800760d835f8f02f8089cba430c4f
keshav halade <keshavhalade2019@gmail.com>
2024-01-31 16:37:22 +0530

    add make sidebar as burger menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,25 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RxDashboard } from "react-icons/rx";
 import { FaBars, FaUserAlt } from "react-icons/fa";
 import { RiHomeHeartFill } from "react-icons/ri";
 import { MdSubject } from "react-icons/md";
 import { Footer } from "../../components/";
-import '../../styles/subcomponents.css'
+import "../../styles/subcomponents.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import { TbLogout2 } from "react-icons/tb";
 import SearchBar from "../searchbar/SearchBar";
 import { TbLogin2 } from "react-icons/tb";
 
-
 function Sidebar({ children }) {
-  console.log('in sidebar');
-  const navigate = useNavigate()
+  console.log("in sidebar");
+  const navigate = useNavigate();
 
   const [isOpen, setIsOpen] = useState(true);
+  const [isMobile, setMobile] = useState(false);
+  // const [isMobileToggle, setIsMobileToggle] = useState(false);
+
   const toggle = () => {
-    setIsOpen(!isOpen);
+     if (window.innerWidth < 760) {
+      setMobile(!isMobile);
+    } else {
+      setIsOpen(!isOpen);
+    }
+     
+    
+    // console.log(isOpen);
+    // setMobile(!isMobile);
   };
+
+  const toggleIfMobile = () => {
+    console.log(isMobile);
+    // setIsMobileToggle(!isMobileToggle)
+    setMobile(!isMobile);
+  };
+
+  // const handleResize = () => {
+  //   setMobile(window.innerWidth < 760);
+  // };
+
+  // useEffect(() => {
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   const openHome = () => {
     window.location.href = "/";
@@ -62,21 +90,25 @@ function Sidebar({ children }) {
           className="link-text primary-white position-relative w-100"
         >
           {item.name}
-          {index === 3 && <span class="badge bg-info position-absolute top-0 ">Beta</span>}
-          {index === 1 && <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
-            <span class="visually-hidden">New alerts</span>
-          </span>}
-
+          {index === 3 && (
+            <span class="badge bg-info position-absolute top-0 ">Beta</span>
+          )}
+          {index === 1 && (
+            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
+              <span class="visually-hidden">New alerts</span>
+            </span>
+          )}
         </div>
       </NavLink>
-    ))
-
+    ));
 
   return (
     <div className="d-flex ">
       <div
         style={{ width: isOpen ? "300px" : "70px" }}
-        className="glass-effect sidebar d-flex flex-column overflow-x-hidden mt-4"
+        className={`glass-effect sidebar d-flex flex-column overflow-x-hidden mt-4  ${
+          isMobile ? "mobile" : "desktop"
+        }`}
       >
         <div className="d-flex align-items-center justify-content-between p-3 pb-2 ">
           <h3
@@ -86,45 +118,77 @@ function Sidebar({ children }) {
           >
             Quizzy
           </h3>
-          <FaBars size={25} className="bars cursor primary-white" onClick={toggle} />
+          <FaBars
+            size={25}
+            className="bars cursor primary-white"
+            onClick={toggle}
+          />
+
+          {/* {isMobile ? (
+            <>
+              <FaBars
+                size={25}
+                className="bars cursor primary-white"
+                onClick={toggleIfMobile}
+              />
+              <span>{children.name}</span>
+            </>
+          ) : (
+            <FaBars
+            size={25}
+            className="bars cursor primary-white"
+            onClick={toggle}
+          />
+          )} */}
+
 
         </div>
         {/* middle horizontal line */}
 
-        <div className=" d-flex flex-column justify-content-between h-100 p-2 border-top">
-          <div>
-            {allMenus()}
-          </div>
+        <div
+          className={`d-flex flex-column justify-content-between h-100 p-2 border-top ${
+            isMobile ? "d-none" : "d-block"
+          }`}
+        >
+          <div>{allMenus()}</div>
 
-          {false ?
-            <div className="d-flex align-items-center gap-3 cursor p-2 login-btn glass-effect"
-              onClick={() => navigate('/signin')}>
+          {false ? (
+            <div
+              className="d-flex align-items-center gap-3 cursor p-2 login-btn glass-effect"
+              onClick={() => navigate("/signin")}
+            >
               <TbLogin2 size={25} />
-              <span>
-                Login
-              </span>
+              <span>Login</span>
             </div>
-            :
-            <div className="d-flex align-items-center gap-3 cursor p-2 logout-btn  glass-effect" >
-
+          ) : (
+            <div className="d-flex align-items-center gap-3 cursor p-2 logout-btn  glass-effect">
               <TbLogout2 size={25} />
-              <span>
-                Logout
-              </span>
-            </div>}
+              <span>Logout</span>
+            </div>
+          )}
         </div>
       </div>
 
-      <div
-        className=" p-4 overflow-y-auto main overflow-x-hidden subcomponent w-100"
-      ><div className="ball-1"></div>
+      <div className=" p-4 overflow-y-auto main overflow-x-hidden subcomponent w-100">
+        <div className="ball-1"></div>
         <div className="ball-2"></div>
         <div className="ball-3"></div>
-        <SearchBar />
-        <div className="glass-effect">
 
-          {children}
-        </div>
+        {isMobile ? (
+          <>
+          <FaBars
+            size={25}
+            className="bars cursor primary-white"
+            onClick={toggleIfMobile}
+          />
+          <span>{children.name}</span>
+          </>
+        ) : (
+          <div></div>
+        )}
+
+        <SearchBar />
+        <div className="glass-effect">{children}</div>
         <Footer />
       </div>
     </div>
