18f7ba7b422d58aaa7570be6333d7a4d2f672da0
Sagar-Thakare <saggythakare01@gmail.com>
2024-02-20 18:51:59 +0530

    side bar open close css updated

--- src/components/sidebar/SagarSidebar.jsx
+++ src/components/sidebar/SagarSidebar.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { Footer } from "../../components/";
-import '../../styles/subcomponents.css'
+import "../../styles/subcomponents.css";
 import "./Sidebar.css";
 import { NavLink, useNavigate } from "react-router-dom";
-import { TbLogout2, TbLogin2} from "react-icons/tb";
+import { TbLogout2, TbLogin2 } from "react-icons/tb";
 import SearchBar from "../searchbar/SearchBar";
 import { MenuItem } from "../../constants";
 import { manageToken } from "../../services";
 import { useAuth } from "../../context/AuthContext";
 
 function SagarSidebar({ children }) {
-  console.log('in sidebar');
-  const navigate = useNavigate()
-  const { user, setUser } = useAuth()
+  console.log("in sidebar");
+  const navigate = useNavigate();
+  const { user, setUser } = useAuth();
   const [isMobile, setMobile] = useState(false);
 
   const [isOpen, setIsOpen] = useState(true);
-
 
   const toggle = () => {
     if (window.innerWidth < 760) {
@@ -61,61 +60,71 @@ function SagarSidebar({ children }) {
   //   ))
 
   const AllMenus = () =>
-  MenuItem.map((item, index) => (
-    <NavLink to={item.path} key={index} className="text-decoration-none rounded-2 d-flex justify-content-center align-items-center p-2 gap-3 menu"  >
-      <div className="icon primary-white d-flex align-items-center">{item.icon}</div>
-
-      <div
-        className={`link-text primary-white position-relative w-100 align-items-center  ${isOpen ? "d-block" : "d-flex"}`}
+    MenuItem.map((item, index) => (
+      <NavLink
+        to={item.path}
+        key={index}
+        className={`text-decoration-none rounded-2 d-flex justify-content-center align-items-center gap-3 menu 
+        ${isOpen ? "p-2" : "p-3"}`}
       >
-        {item.name}
-        {/* {index === 3 && (
-          <span className="badge bg-info position-absolute top-0 ">Beta</span>
-        )}
-        {index === 1 && (
-          <span className="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
-            <span className="visually-hidden">New alerts</span>
-          </span>
-        )} */}
-      </div>
-    </NavLink>
-  ))
-    // return login/logout button according to scenario
+        <div className="primary-white d-flex align-items-center">
+          {item.icon}
+        </div>
+
+        <div
+          className={`menu-text primary-white position-relative w-100 align-items-center
+           ${isOpen ? "d-flex" : "d-none"}`}
+        >
+          {item.name}
+        </div>
+      </NavLink>
+    ));
+    
+  // return login/logout button according to scenario
   const IsUserLoggedIn = () => {
-    return <div>
+    return (
+      <div>
+        <div
+          className={`d-flex align-items-center  gap-3 cursor p-2 glass-effect  
+            ${user ? "logout-btn" : "login-btn"} ${!isOpen && "justify-content-center"}`}
+          onClick={handleUser}
+          title={user ? `Logout as ${user.name}` : `Login`}
+        >
+          {user ? (
+            <>
+              <TbLogout2 size={25} />
 
-      <div
-        className={`d-flex align-items-center gap-3 cursor p-2 glass-effect ${user ? 'logout-btn' : 'login-btn'}`}
-        onClick={handleUser}
-        title={user ? `Logout as ${user.name}` : `Login`}
-      >
-
-        {user ?
-          <><TbLogout2 size={25} /> {user.name}</>
-          :
-          <><TbLogin2 size={25} /> Login </>}
-
-
+              <span className={`${isOpen ? "d-flex" : "d-none"}`}>
+                {user.name}
+              </span>
+            </>
+          ) : (
+            <>
+              <TbLogin2 size={25} /> Login{" "}
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  }
+    );
+  };
 
   // to handle user's token nad navigation
   const handleUser = () => {
     if (user) {
-      manageToken('delete', "token")
-      setUser(null)
+      manageToken("delete", "token");
+      setUser(null);
     } else {
-      navigate('/signin')
+      navigate("/signin");
     }
-  }
+  };
 
   return (
     <div className="d-flex ">
       <div
         style={{ width: isOpen ? "300px" : "50px " }}
-        className={`glass-effect sidebar d-flex flex-column overflow-x-hidden mt-4  ${isMobile ? "mobile" : "desktop"
-          }`}
+        className={`glass-effect sidebar d-flex flex-column overflow-x-hidden mt-4  ${
+          isMobile ? "mobile" : "desktop"
+        }`}
       >
         <div className="d-flex align-items-center justify-content-between p-3 pb-2 ">
           <h3
@@ -125,14 +134,18 @@ function SagarSidebar({ children }) {
           >
             Quizzy
           </h3>
-          <FaBars size={25} className="bars cursor primary-white" onClick={toggle} />
-
+          <FaBars
+            size={25}
+            className="bars cursor primary-white"
+            onClick={toggle}
+          />
         </div>
         {/* middle horizontal line */}
 
         <div
-          className={`d-flex flex-column justify-content-between h-100 p-2 border-top ${isMobile ? "d-none" : "d-block"
-            }`}
+          className={`d-flex flex-column justify-content-between h-100 p-2 border-top ${
+            isMobile ? "d-none" : "d-block"
+          }`}
         >
           <div>
             <AllMenus />
@@ -142,9 +155,8 @@ function SagarSidebar({ children }) {
         </div>
       </div>
 
-      <div
-        className=" p-4 overflow-y-auto main overflow-x-hidden subcomponent w-100"
-      ><div className="ball-1"></div>
+      <div className=" p-4 overflow-y-auto main overflow-x-hidden subcomponent w-100">
+        <div className="ball-1"></div>
         <div className="ball-2"></div>
         <div className="ball-3"></div>
 
@@ -162,10 +174,7 @@ function SagarSidebar({ children }) {
         )}
 
         <SearchBar />
-        <div className="glass-effect">
-
-          {children}
-        </div>
+        <div className="glass-effect">{children}</div>
         <Footer />
       </div>
     </div>
