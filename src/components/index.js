7c7bf58922d3ff340e73fa5217567e9a7c5023d1
AM-SagarThakare <sagar.thakare@angularminds.in>
2024-02-29 22:29:50 +0530

    unwanted code removed

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as SearchBar } from "./searchbar/SearchBar";
-export { default as Sidebar } from "./sidebar/Sidebar";
-export { default as Carousel } from './carousel/Carousel';
+export { default as Sidebar } from "./sidebar/deleteSidebar";
+export { default as Carousel } from './deletecarousel/Carousel';
 export { default as Footer } from './footer/Footer';
 export { default as Card } from './card/Card';
 export { default as LabelledInput } from './LabelledInput';
