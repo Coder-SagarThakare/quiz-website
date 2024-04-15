327c86e876d678a0ae6e40325753c5b18c5b46bc
Sagar-Thakare <saggythakare01@gmail.com>
2024-04-15 18:32:08 +0530

    cards arranged

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,12 +2,17 @@ import React from "react";
 // import { subjectBackgrounds } from "../../utils/Data";
 
 const Card = ({ name, backgroundImage }) => {
-
   return (
-    <div className="position-relative border rounded-4 cursor p-2"
-      style={{ backgroundImage: `url(${backgroundImage})`, height: "200px", backgroundSize : "cover" ,}}
+    <div
+      className=" position-relative border rounded-4 cursor overflow-hidden"
+      style={{ height: "200px" }}
     >
-      <p className="position-absolute bottom-0 bg-primary w-75 px-3 py-2 rounded-4">{name}</p>
+      <img src={backgroundImage} alt={backgroundImage} className="w-100" />
+
+      <div className="empty-card position-absolute top-0 bottom-0 right-0 left-0 w-100 "></div>
+      <p className="position-absolute bottom-0 w-100 text-light ps-3 pb-2 m-0">
+        {name}
+      </p>
     </div>
   );
 };
