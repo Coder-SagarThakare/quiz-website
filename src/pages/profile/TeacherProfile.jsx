3cfbcc42891ad245ccbf351c68e73503a161328c
Coder-SagarThakare <saggythakare01@gmail.com>
2024-06-24 01:21:47 +0530

    working on teacher profile UI

--- src/pages/profile/TeacherProfile.jsx
+++ src/pages/profile/TeacherProfile.jsx
@@ -17,7 +17,7 @@ function TeacherProfile() {
 
       setUser(data);
       console.log(data);
-    } catch (err) {}
+    } catch (err) { }
   }
 
   useEffect(() => {
@@ -26,33 +26,70 @@ function TeacherProfile() {
   }, []);
 
   return (
-    <>
-      <div className="d-flex w-100 p-3 gap-5 ">
+    <div className="p-3">
+
+      {/* profile data card */}
+      <div className="glass-effect d-flex align-items-center w-100 p-3">
+
+        {/* profile photo div */}
         <div className="border p-1 rounded-pill overflow-hidden profile-img">
-          <img
+          {/* <img
             src={user.picture}
             alt={user.picture}
             className="w-100 h-100 rounded-pill"
-          />
+          /> */}
         </div>
+
+        {/* profile info */}
+        <div className="border d-flex p-1 " >
+
+          <div className="border ">
+
+            {edit ? (
+              <input type="text" defaultValue={user.name} />
+            ) : (
+              <p> {user.name} </p>
+            )}
+            <p> {user.email}</p>
+            <p> {user.mobNo}</p>
+          </div>
+
+          <div className="border ">
+            <div className="d-flex gap-2">
+              <div>
+                <Link to={user.github} target="_blank">
+                  <FaGithub />{" "}
+                </Link>
+              </div>
+              <div>
+                <Link to={user.linkedin} target="_blank">
+                  <FaLinkedin />{" "}
+                </Link>
+              </div>
+            </div>
+            <p>{user.gender}</p>
+            {edit ? (
+            <input type="date" value={user.birthDate.slice(0, 10)} />
+          ) : (
+            <p> {user.birthDate.slice(0, 10)}</p>
+          )}
+          </div>
+        </div>
+ 
+      </div>
+
+      {/* delete below div */}
+      <div className="d-flex w-100 p-3 gap-5 ">
+
         <div className=" ">
           <FaUserEdit size={50} onClick={() => setEdit(!edit)} />
           <br />
           Mr/Ms{" "}
-          {edit ? (
-            <input type="text" defaultValue={user.name} />
-          ) : (
-            <p> {user.name} </p>
-          )}
-          <p>Email-ID : {user.email}</p>
-          <p>Mobile : {user.mobNo}</p>
-          <p>Gender : {user.gender}</p>
+
+
+          
           <p>Bio : {user.bio}</p>
-          {edit ? (
-            <input type="date" value={user.birthDate.slice(0, 10)} />
-          ) : (
-            <p>Birth Date : {user.birthDate.slice(0, 10)}</p>
-          )}
+          
           <p>Organization : {user.organization}</p>
           <p>Highest Education : {user.highestEducation}</p>
           <div className="d-flex gap-4">
@@ -67,26 +104,15 @@ function TeacherProfile() {
           <p>Role : {user.role}</p>
           <p>Teaching Experience : {user.teachingExperience}</p>
           <p>specialization : {user.specialization}</p>
-          
+
           <p>Email verified : {user.isEmailVerified ? "yes" : "no"}</p>
           <p>isTeacherVerfied : {user.isTeacherVerfied ? "yes" : "no"}</p>
           <p>isPasswordUpdated : {user.isPasswordUpdated ? "yes" : "no"}</p>
-          <div className="d-flex gap-2">
-            <div>
-              <Link to={user.github} target="_blank">
-                <FaGithub />{" "}
-              </Link>
-            </div>
-            <div>
-              <Link to={user.linkedin} target="_blank">
-                <FaLinkedin />{" "}
-              </Link>
-            </div>
-          </div>
+
           <p>Acoount created At : {user.createdAt.slice(0, 10)}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
