7c82f51ef87005e8ca5c99494ba7a14549639e74
Sagar-Thakare <saggythakare01@gmail.com>
2024-06-28 19:00:06 +0530

    profile updated

--- src/pages/profile/TeacherProfile.jsx
+++ src/pages/profile/TeacherProfile.jsx
@@ -17,7 +17,7 @@ function TeacherProfile() {
 
       setUser(data);
       console.log(data);
-    } catch (err) { }
+    } catch (err) {}
   }
 
   useEffect(() => {
@@ -26,36 +26,29 @@ function TeacherProfile() {
   }, []);
 
   return (
-    <div className="p-3">
-
+    <div className="p-3  ">
       {/* profile data card */}
-      <div className="glass-effect d-flex align-items-center w-100 p-3">
-
+      <div className="glass-effect row align-items-center p-3 gx-0">
         {/* profile photo div */}
-        <div className="border p-1 rounded-pill overflow-hidden profile-img">
-          {/* <img
+        <div className=" p-1  col-2  overflow-hidden">
+          <img
             src={user.picture}
             alt={user.picture}
-            className="w-100 h-100 rounded-pill"
-          /> */}
+            className="w-100 h-100 rounded-pill profile-img  m-1 float-center "
+          />
         </div>
 
         {/* profile info */}
-        <div className="border d-flex p-1 " >
-
-          <div className="border ">
-
-            {edit ? (
-              <input type="text" defaultValue={user.name} />
-            ) : (
-              <p> {user.name} </p>
-            )}
-            <p> {user.email}</p>
-            <p> {user.mobNo}</p>
-          </div>
-
-          <div className="border ">
-            <div className="d-flex gap-2">
+        <div className=" p-0 col-md-10 row border">
+          <div className="row col-12">
+            <div className="col-6 gx-0">
+              {edit ? (
+                <input type="text" defaultValue={user.name} />
+              ) : (
+                <p> {user.name} </p>
+              )}
+            </div>
+            <div className="d-flex gap-2 col-6 gx-0">
               <div>
                 <Link to={user.github} target="_blank">
                   <FaGithub />{" "}
@@ -67,29 +60,32 @@ function TeacherProfile() {
                 </Link>
               </div>
             </div>
+          </div>
+          
+          <div className="col-6 gx-0">
+            <p> {user.email}</p>
+            <p> {user.mobNo}</p>
+          </div>
+              
+          <div className=" col-6 gx-0">
             <p>{user.gender}</p>
             {edit ? (
-            <input type="date" value={user.birthDate.slice(0, 10)} />
-          ) : (
-            <p> {user.birthDate.slice(0, 10)}</p>
-          )}
+              <input type="date" value={user.birthDate.slice(0, 10)} />
+            ) : (
+              <p> {user.birthDate.slice(0, 10)}</p>
+            )}
           </div>
         </div>
- 
+
+        <p className="ms-3">Bio : {user.bio}</p>
       </div>
 
       {/* delete below div */}
       <div className="d-flex w-100 p-3 gap-5 ">
-
         <div className=" ">
           <FaUserEdit size={50} onClick={() => setEdit(!edit)} />
           <br />
-          Mr/Ms{" "}
-
-
-          
-          <p>Bio : {user.bio}</p>
-          
+          Mr/Ms
           <p>Organization : {user.organization}</p>
           <p>Highest Education : {user.highestEducation}</p>
           <div className="d-flex gap-4">
@@ -100,15 +96,12 @@ function TeacherProfile() {
               style={{ width: "500px" }}
             />
           </div>
-
           <p>Role : {user.role}</p>
           <p>Teaching Experience : {user.teachingExperience}</p>
           <p>specialization : {user.specialization}</p>
-
           <p>Email verified : {user.isEmailVerified ? "yes" : "no"}</p>
           <p>isTeacherVerfied : {user.isTeacherVerfied ? "yes" : "no"}</p>
           <p>isPasswordUpdated : {user.isPasswordUpdated ? "yes" : "no"}</p>
-
           <p>Acoount created At : {user.createdAt.slice(0, 10)}</p>
         </div>
       </div>
