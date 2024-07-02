89f65ff622f74a5fd3e8ea43f9ecc7ff5e13cddc
Sagar-Thakare <saggythakare01@gmail.com>
2024-07-02 18:35:52 +0530

    teacher profile UI chaned

--- src/pages/profile/TeacherProfile.jsx
+++ src/pages/profile/TeacherProfile.jsx
@@ -28,52 +28,54 @@ function TeacherProfile() {
   return (
     <div className="p-3  ">
       {/* profile data card */}
-      <div className="glass-effect row align-items-center p-3 gx-0">
+      <div className="glass-effect row align-items-center justify-content-between p-3 gx-0 border ">
         {/* profile photo div */}
-        <div className=" p-1  col-2  overflow-hidden">
+        <div className=" col-12 col-lg-2 text-center border ">
           <img
             src={user.picture}
             alt={user.picture}
-            className="w-100 h-100 rounded-pill profile-img  m-1 float-center "
+            className="w-100 h-100 rounded-pill profile-img float-center border m-1"
           />
         </div>
 
         {/* profile info */}
-        <div className=" p-0 col-md-10 row border">
-          <div className="row col-12">
-            <div className="col-6 gx-0">
+        <div className=" p-0  col-xl-9 col-xxl-10 gx-0 row text-md-start text-lg-start border align-items-baseline"> 
+          <div className="row col-12 gx-0 border">
+            <div className="col-6 gx-0  border border-danger">
               {edit ? (
                 <input type="text" defaultValue={user.name} />
               ) : (
                 <p> {user.name} </p>
               )}
             </div>
-            <div className="d-flex gap-2 col-6 gx-0">
-              <div>
+            <div className="d-flex gap-2 col-6 gx-0 border border-danger text-center d-flex ">
+              <>
                 <Link to={user.github} target="_blank">
                   <FaGithub />{" "}
                 </Link>
-              </div>
-              <div>
+              </>
+              <>
                 <Link to={user.linkedin} target="_blank">
                   <FaLinkedin />{" "}
                 </Link>
-              </div>
+              </>
             </div>
-          </div>
-          
-          <div className="col-6 gx-0">
-            <p> {user.email}</p>
-            <p> {user.mobNo}</p>
-          </div>
-              
-          <div className=" col-6 gx-0">
-            <p>{user.gender}</p>
-            {edit ? (
-              <input type="date" value={user.birthDate.slice(0, 10)} />
-            ) : (
-              <p> {user.birthDate.slice(0, 10)}</p>
-            )}
+          </div>  
+          <div className="row gx-0 border">
+
+            <div className="col-6 gx-0 border border-success">
+              <p> {user.email}</p>
+              <p> {user.mobNo}</p>
+            </div>
+
+            <div className=" col-6 gx-0 border border-success">
+              <p>{user.gender}</p>
+              {edit ? (
+                <input type="date" value={user.birthDate.slice(0, 10)} />
+              ) : (
+                <p> {user.birthDate.slice(0, 10)}</p>
+              )}
+            </div>
           </div>
         </div>
 
