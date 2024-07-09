c51a9ac1e325c51ebc3aa6af5e89423d85f0e1b1
Coder-SagarThakare <saggythakare01@gmail.com>
2024-07-10 02:00:37 +0530

    teacher profile updated

--- src/pages/profile/TeacherProfile.jsx
+++ src/pages/profile/TeacherProfile.jsx
@@ -19,7 +19,7 @@ function TeacherProfile() {
 
       setUser(data);
       console.log(data);
-    } catch (err) {}
+    } catch (err) { }
   }
 
   useEffect(() => {
@@ -27,21 +27,36 @@ function TeacherProfile() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const GetSocialMedia = () => {
+    return (<>
+      <>
+        <Link to={user.github} target="_blank">
+          <FaGithub size={25}/>{" "}
+        </Link>
+      </>
+      <>
+        <Link to={user.linkedin} target="_blank">
+          <FaLinkedin size={25}/>{" "}
+        </Link>
+      </>
+    </>)
+  }
+
   return (
     <div className="p-3 d-block">
 
-        <div className="d-flex justify-content-end border">
+      <div className="d-flex justify-content-end border">
         {edit ?
-          <Button title="Save" style={{width: "100px"}} className={"bg-info"} onClick={() => setEdit(!edit)}/>:
-          <Button title="Edit"  style={{width: "100px"}} className={"bg-primary"} onClick={() => setEdit(!edit)} /> 
+          <Button title="Save" style={{ width: "100px" }} className={"bg-info"} onClick={() => setEdit(!edit)} /> :
+          <Button title="Edit" style={{ width: "100px" }} className={"bg-primary"} onClick={() => setEdit(!edit)} />
         }
-        </div>
+      </div>
 
       {/* profile data card */}
-      <div className="glass-effect row align-items-center justify-content-between p-3 gx-0 border ">
+      <div className="glass-effect row align-items-start align-items-xl-center justify-content-between p-3 gx-0 border text-break">
         {/* profile photo div */}
-        <div className=" col-12  col-lg-2 text-center border ">
-          <img  
+        <div className=" col-12 col-md-4 col-lg-2 text-center border ">
+          <img
             src={user.picture}
             alt={user.picture}
             className="w-100 h-100 rounded-pill profile-img float-center border m-1"
@@ -49,11 +64,11 @@ function TeacherProfile() {
         </div>
 
         {/* profile info */}
-        <div className=" p-0 col-lg-9 col-xl-9 me-xl-5 gx-0 row border border-success border-1 p-1">
+        <div className=" p-0 col-md-8 col-lg-9 col-xl-9 me-xl-5 gx-0 row border border-success border-1 p-1">
           <div className="d-none d-sm-block col-2 d-md-none"></div>
 
           <div className="row col-12 col-sm-10 col-md-12 gx-0 border  p-1">
-            <div className="col-12 gx-0 col-md-8 col-xl-6 border border-danger ">  
+            <div className="col-12 gx-0 col-lg-8 col-xl-6 border border-danger ">
               {edit ? (
                 <input type="text" defaultValue={user.name} />
               ) : (
@@ -64,37 +79,28 @@ function TeacherProfile() {
               )}
             </div>
 
-            <div className="d-none d-md-flex gap-2 col-12 col-md-4 col-xl-6 gx-0 border border-danger text-center d-flex ">
-              
-              <>
-                <Link to={user.github} target="_blank">
-                  <FaGithub />{" "}
-                </Link>
-              </>
-              <>
-                <Link to={user.linkedin} target="_blank">
-                  <FaLinkedin />{" "}
-                </Link>
-              </>
+            <div className="d-none d-lg-flex gap-2 col-12 col-md-4 col-xl-6 gx-0 border border-danger text-center d-flex ">
+              <GetSocialMedia />
+
             </div>
           </div>
 
           <div className="row gx-0 border">
-          <div className="d-none d-sm-block col-2 d-md-none gx-0 border border-danger row">  </div>
-            
-            <div className="col-12 col-sm-10 col-md-8 col-xl-6 gx-0 border border-success">
+            <div className="d-none d-sm-block col-2 d-md-none gx-0 border border-danger row">  </div>
+
+            <div className="col-12 col-sm-10 col-md-12 col-lg-8 col-xl-6 gx-0 border border-success">
               <p>
-                <Mail size={20} /> sagar.thakare@angularminds.com
+                <Mail size={20} /> sagar.thakare@angularminds.com-pleasecChangeThis
               </p>
               <p>
                 {" "}
                 <Phone size={20} /> {user.mobNo}
               </p>
             </div>
-            
+
             <div className="d-none d-sm-block col-2 d-md-none gx-0  border border-danger row">  </div>
 
-            <div className="col-12 col-sm-10 col-md-4 col-xl-6 gx-0 border border-success">
+            <div className="col-12 col-sm-12 col-lg-4 col-xl-6 gx-0 border border-success">
               <p>
                 {" "}
                 <User size={20} /> {user.gender}
@@ -112,15 +118,15 @@ function TeacherProfile() {
 
         </div>
 
-        <div className="d-none d-sm-block col-2 d-md-none gx-0  border border-danger row">  </div>
+        <div className="d-none d-md-block col-4 col-lg-3 col-xl-1 gx-0  border border-danger row">  </div>
 
-        <p className="col-10 ms-sm-4">Bio : {user.bio}</p>
+        <p className="col-8 col-lg-9 col-xl-11 border border-danger">Bio : {user.bio}</p>
       </div>
 
       {/* delete below div */}
       <div className="d-flex w-100 p-3 gap-5 ">
         <div className=" ">
-          <FaUserEdit size={50}  />
+          <FaUserEdit size={50} />
           <br />
           Mr/Ms
           <p>Organization : {user.organization}</p>
