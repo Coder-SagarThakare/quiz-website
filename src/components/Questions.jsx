d2864976c85f105fcd54007d624a18bede3a43bb
Sagar-Thakare <sagar.thakare@angularminds.in>
2024-09-30 14:44:52 +0530

    Timer component added

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect,useState } from "react";
 import { useLocation } from "react-router-dom";
 import { get } from "../services";
-import { apiPaths, CONSTANTS } from "../constants";
+import { apiPaths } from "../constants";
 import Loader from "./Loader";
 import Question from "./Question";
 import Button from "./custom/Button";
+import Timer from "./custom/Timer";
 
 function Questions() {
   const [questionsArr, setQuestionsArr] = useState([]);
   const location = useLocation();
   const [loading, setLoading] = useState(true);
-  const [testTiming, setTestTiming] = useState(CONSTANTS.TEST_TIME_IN_MINUTES);
   const [currentQuestionNo, setCurrentQuestionNo] = useState(0);
 
   async function getAllQuestions() {
@@ -18,7 +18,8 @@ function Questions() {
       const questionsArr = await get(
         `${apiPaths.STUDENT.QUESTIONS_BY_TOPIC}`.replace(
           "topicId?level={level}",
-          `${location.state.topicId
+          `${
+            location.state.topicId
           }?level=${location.state.level.toLowerCase()}`
         )
       );
@@ -51,37 +52,13 @@ function Questions() {
     // eslint-disable-next-line
   }, []);
 
-  useEffect(() => {
-    let time = CONSTANTS.TEST_TIME_IN_MINUTES * 60; // Total time in seconds
-    const timeInterval = setInterval(() => {
-      if (time > 0) {
-        time--; // Decrease time by 1 second each interval
-
-        // Calculate minutes and seconds
-        const minutes = Math.floor(time / 60);
-        const seconds = time % 60;
-
-        // Print minutes and seconds with proper formatting
-        console.log(`${minutes}:${seconds < 10 ? '0' + seconds : seconds}`);
-        // setTestTiming(`${minutes}:${seconds < 10 ? '0' + seconds : seconds}`);
-
-      } else {
-        clearInterval(timeInterval); // Stop the timer when it reaches 0
-        console.log("Time's up!");
-      }
-    }, 1000);
-
-    // Cleanup the interval when the component unmounts
-    return () => clearInterval(timeInterval);
-  }, []);
-
-  const submitTest = ()=>{
+  const submitTest = () => {
     console.log("TEST SUBMITTED...");
-  }
+  };
 
   if (loading) {
     <Loader />;
-  }               
+  }
 
   console.log(questionsArr);
 
@@ -90,8 +67,16 @@ function Questions() {
       <div>
         <div className="d-flex justify-content-between align-items-center w-100 rounded py-2 px-3 question-header primary-white my-2">
           <span>Topic Name : Data types</span>
-          <span>Time : {testTiming}</span>
-          <Button title={"Submit"}  className={"bg-success"} onClick={submitTest}/>
+          <div>
+            <span>Time </span>
+            <Timer />
+          </div>
+
+          <Button
+            title={"Submit"}
+            className={"bg-success"}
+            onClick={submitTest}
+          />
         </div>
 
         {/* questions */}
---
+++ src/components/custom/Timer.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { CONSTANTS } from "../../constants";
+
+function Timer() {
+  const [testTiming, setTestTiming] = useState(CONSTANTS.TEST_TIME_IN_MINUTES);
+
+  useEffect(() => {
+    let time = CONSTANTS.TEST_TIME_IN_MINUTES * 60; // Total time in seconds
+    const timeInterval = setInterval(() => {
+      if (time > 0) {
+        time--; // Decrease time by 1 second each interval
+
+        // Calculate minutes and seconds
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+
+        // Print minutes and seconds with proper formatting
+        setTestTiming(`${minutes}:${seconds < 10 ? "0" + seconds : seconds}`);
+        // counterRef.current = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+      } else {
+        clearInterval(timeInterval); // Stop the timer when it reaches 0
+        console.log("Time's up!");
+      }
+    }, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(timeInterval);
+  }, []);
+
+  return <span className={`${testTiming < 5 ? "text-danger" : "text-success"}`}>{testTiming}</span>;
+}
+
+export default Timer;
