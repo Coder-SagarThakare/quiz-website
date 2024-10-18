526494aaad2074854178d9726f0bd2331f1579f1
Sagar-Thakare <sagar.thakare@angularminds.in>
2024-10-18 10:19:18 +0530

    outlet added in answer provider

--- src/components/TestResult.jsx
+++ src/components/TestResult.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useAnswers } from "../context/AnswersContext";
 
 function TestResult() {
+  const { answers } = useAnswers();
+
+  console.log("answer", answers);
+
   return (
     <div>
-        <h1>Congragulations</h1>
-        <div>5/10</div>
+      <h1>Congragulations</h1>
+      <div>5/10</div>
     </div>
-  )
+  );
 }
 
-export default TestResult
+export default TestResult;
--- src/context/AnswersContext.jsx
+++ src/context/AnswersContext.jsx
@@ -1,19 +1,19 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from "react";
+import { Outlet } from "react-router-dom";
 
 const AnswersContext = createContext();
 
 export function AnswersProvider({ children }) {
+  console.log("children", children);
+  const [answers, setAnswers] = useState([{ name: "sagar" }]);
 
-    console.log("children", children)
-    const [answers, setAnswers] = useState([])
-
-    return (
-        <AnswersContext.Provider value={{ answers, setAnswers }}>
-            {children}
-        </AnswersContext.Provider>
-    )
+  return (
+    <AnswersContext.Provider value={{ answers, setAnswers }}>
+      <Outlet />
+    </AnswersContext.Provider>
+  );
 }
 
 export const useAnswers = () => {
-    return useContext(AnswersContext)
-}
+  return useContext(AnswersContext);
+};
